4f6cd6d3c9f93e558705a7b654709ae779320071
Mawele <mbizi.kevin@gmail.com>
2025-11-17 20:11:58 +0100

    create resolver for skill

--- src/app/features/experiences/services/skill/skill.store.ts
+++ src/app/features/experiences/services/skill/skill.store.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
+import { SkillItem } from '../../models/skill-item.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SkillStore {
-  
+  private skillItemDevWebSignal = signal<SkillItem[] | undefined>(undefined);
+  private skillItemDevMobileSignal = signal<SkillItem[] | undefined>(undefined);
+  private skillItemDevGameSignal = signal<SkillItem[] | undefined>(undefined);
+  private skillItemOtherSignal = signal<SkillItem[] | undefined>(undefined);
+
+  skillItemDevWeb = computed(() => this.skillItemDevWebSignal());
+  skillItemDevMobile = computed(() => this.skillItemDevMobileSignal());
+  skillItemDevGame = computed(() => this.skillItemDevGameSignal());
+  skillItemDevOther = computed(() => this.skillItemOtherSignal());
+
+  loadSkillDevWeb(newSkillArray: SkillItem[]) {
+    this.skillItemDevGameSignal.set(newSkillArray);
+  }
+
+  loadSkillDevMobile(newSkillArray: SkillItem[]) {
+    this.skillItemDevMobileSignal.set(newSkillArray);
+  }
+
+  loadSkillDevGame(newSkillArray: SkillItem[]) {
+    this.skillItemDevGameSignal.set(newSkillArray);
+  }
+
+  loadSkillOther(newSkillArray: SkillItem[]) {
+    this.skillItemOtherSignal.set(newSkillArray);
+  }
 }
